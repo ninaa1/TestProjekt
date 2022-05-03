8c2ce53400b7beb25aebe1db0ed8ed97f7073620
ninaa1 <nikolinacolic18@gmai.com>
2022-05-03 14:38:55 +0200

    dodana varijabla

--- testProjekt/Employee/src/main/java/net/codejava/Employee.java
+++ testProjekt/Employee/src/main/java/net/codejava/Employee.java
@@ -15,7 +15,7 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy =  GenerationType.IDENTITY)
 	private long id;
-	
+	private String nn;
 	@Column(name = "name")
 	private String firstName;
 	
